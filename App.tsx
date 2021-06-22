6c5cf40104c6c31075a8c160ea7fe361d0e82745
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-06-22 07:32:53 -0400

    First Day

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/Signin'
 
-export default function App() {
+
+
+
+export default function App(){
+  // const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SignIn />
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
