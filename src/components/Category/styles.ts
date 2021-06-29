69468ce8b0b0f483f36ab11b245dcf7e7466ea80
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-06-28 22:06:57 -0400

    ThirdDay, created AppointDetails interface, Header and Member components

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -17,32 +17,33 @@ export const styles = StyleSheet.create({
         borderRadius: 8,
         alignItems: 'center',
         justifyContent:'space-between',
-        paddingVertical: 7
+        paddingVertical: 20
 
     },
     checked:{
+        position: 'absolute',
+        top: 7,
+        right: 7,
         width: 12,
         height: 12,
-        backgroundColor: theme.colors.primary,
-        alignSelf: 'flex-end',
-        marginRight: 7,
-        borderRadius: 3
-       
+        backgroundColor: theme.colors.primary,         
+        borderRadius: 3,    
 
     }, 
     unchecked:{
+      position: 'absolute',
+      top: 7,
+      right: 7,
       width: 10,
       height: 10,
-      backgroundColor: theme.colors.secondary100,
-      alignSelf: 'flex-end',
-      marginRight: 7,
+      backgroundColor: theme.colors.secondary100,          
       borderColor: theme.colors.secondary50, 
       borderWidth: 2,
       borderRadius: 3
 
     }, 
     title:{
-        fontFamily: theme.fonts.title500,
+        fontFamily: theme.fonts.title700,
         color: theme.colors.heading,
         fontSize: 15
     }
