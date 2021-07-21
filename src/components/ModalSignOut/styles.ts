c402ffc87207946a10562076fb58971f43f56b44
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-07-20 22:25:06 -0400

    Added ModalSignOut component

---
+++ src/components/ModalSignOut/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  overlay:{
+      flex:1,
+      backgroundColor: theme.colors.overlay,
+      justifyContent: 'flex-end', 
+ 
+  },  
+  container: { 
+    height: 170,  
+    marginTop:26,
+  },
+  title:{
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginBottom: 26
+        
+  },
+  titleDetails: {
+    fontFamily: theme.fonts.title700,
+    fontSize: 20,
+    color: theme.colors.heading
+  },
+  titleComponentDetails: {
+    fontFamily: theme.fonts.title700,
+    fontSize: 20,
+    color: theme.colors.primary
+  },
+  buttonsContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      paddingHorizontal: 24
+  }
+ 
+});
