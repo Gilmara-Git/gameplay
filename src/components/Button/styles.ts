7b59e354f1f9c8906da603fde551384496de1817
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-07-20 22:27:03 -0400

    Corrected style property from border to borderWidth

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -8,7 +8,9 @@ export const styles =  StyleSheet.create({
         backgroundColor: theme.colors.primary,
         borderRadius: 8, 
         flexDirection: 'row',
-        alignItems: 'center', 
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.colors.primary 
     },    
     title:{
         flex:1,
